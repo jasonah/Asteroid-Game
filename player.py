91693fb75a0a8f8d40f8b119920293689311b9f4
jasonah <jasonh1028@gmail.com>
2026-07-07 15:50:19 -0500

    added shooting mechanics

--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -21,6 +22,10 @@ class Player(CircleShape):
 
     def rotate(self, dt) -> None:
         self.rotation += PLAYER_TURN_SPEED * dt
+
+    def shoot(self) -> None:
+        Shot(self.position.x, self.position.y).velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
     
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
@@ -33,6 +38,8 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt: float) -> None:
         unit_vector = pygame.Vector2(0, 1)
